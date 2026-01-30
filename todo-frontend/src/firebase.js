0b82c00053f5c7c2cb0c0ffe3e5f15a144e423f4
Naman Khandelwal <129101844+naman7727@users.noreply.github.com>
2026-01-30 23:12:51 +0530

    Modify Firebase configuration details
    Updated Firebase configuration for the project.

--- todo-frontend/src/firebase.js
+++ todo-frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1UmRpSMTCjIWv-gtI6MolcJ8DVDRaNFY",
-  authDomain: "todo-app-bc853.firebaseapp.com",
-  projectId: "todo-app-bc853",
- 
-  appId: "1:543676399988:web:1e48093170fb647484ac21",
+  apiKey: "AIzaSyD1UmRpSMTC",//demo
+  authDomain: "todo-app-3.firebaseapp.com",//demo
+  projectId: "todo-app-3", //demo
+  appId: "1:546399988:web:", // demo
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
